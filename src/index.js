d1adc73116bf3e8f1763c2bd007a73d249f00fd6
Vitaliy <vetal.morenko@mail.ru>
2020-10-25 19:16:54 +0300

    feat: add decision for task morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,29 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(arr) {
+    const splitArr = arr.split('**********');
+    const result = [];
+    splitArr.forEach( item => {
+        const words = [];
+        for (let i = 0; i < item.length / 10; i++) {
+            let itemSlice = item.slice(10 * i, 10 * (i + 1));
+            let letter = '';
+            for (let k = 0; k < 5; k++) {
+                const character = itemSlice.slice(0, 2);
+                itemSlice = itemSlice.slice(2);
+                if (character === '10') {
+                    letter += '.';
+                } else if (character === '11') {
+                    letter += '-';
+                }
+            }
+            words.push(MORSE_TABLE[letter]);
+        }
+        result.push(words.join(''));
+    });
+
+    return result.join(' ');
 }
 
 module.exports = {
